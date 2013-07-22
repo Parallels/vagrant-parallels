c198902ba4b0a17a6cbc9d6bdd9e54fff549dfa4
Youssef Shahin <yshahin@gmail.com>
2013-07-22 17:35:09 +0200

    clean up start fresh better

--- lib/vagrant-parallels/action.rb
+++ lib/vagrant-parallels/action.rb
@@ -11,27 +11,6 @@ module VagrantPlugins
       # a bootup (i.e. not saved).
       def self.action_boot
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckAccessible
-          # b.use CleanMachineFolder
-          # b.use SetName
-          # b.use ClearForwardedPorts
-          # b.use Provision
-          # b.use EnvSet, :port_collision_repair => true
-          # b.use PrepareForwardedPortCollisionParams
-          # b.use HandleForwardedPortCollisions
-          # b.use PruneNFSExports
-          # b.use NFS
-          # b.use PrepareNFSSettings
-          # b.use ClearSharedFolders
-          # b.use ShareFolders
-          # b.use ClearNetworkInterfaces
-          # b.use Network
-          # b.use ForwardPorts
-          # b.use SetHostname
-          # b.use SaneDefaults
-          # b.use Customize
-          # b.use Boot
-          # b.use CheckGuestAdditions
         end
       end
 
@@ -39,28 +18,6 @@ module VagrantPlugins
       # freeing the resources of the underlying virtual machine.
       def self.action_destroy
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use Call, Created do |env1, b2|
-          #   if !env1[:result]
-          #     b2.use MessageNotCreated
-          #     next
-          #   end
-
-          #   b2.use Call, DestroyConfirm do |env2, b3|
-          #     if env2[:result]
-          #       b3.use ConfigValidate
-          #       b3.use CheckAccessible
-          #       b3.use EnvSet, :force_halt => true
-          #       b3.use action_halt
-          #       b3.use PruneNFSExports
-          #       b3.use Destroy
-          #       b3.use CleanMachineFolder
-          #       b3.use DestroyUnusedNetworkInterfaces
-          #     else
-          #       b3.use MessageWillNotDestroy
-          #     end
-          #   end
-          # end
         end
       end
 
@@ -68,72 +25,18 @@ module VagrantPlugins
       # the virtual machine, gracefully or by force.
       def self.action_halt
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use Call, Created do |env, b2|
-          #   if env[:result]
-          #     b2.use CheckAccessible
-          #     b2.use DiscardState
-
-          #     b2.use Call, IsPaused do |env2, b3|
-          #       next if !env2[:result]
-          #       b3.use Resume
-          #     end
-
-          #     b2.use Call, GracefulHalt, :poweroff, :running do |env2, b3|
-          #       if !env2[:result]
-          #         b3.use ForcedHalt
-          #       end
-          #     end
-          #   else
-          #     b2.use MessageNotCreated
-          #   end
-          # end
         end
       end
 
       # This action packages the virtual machine into a single box file.
       def self.action_package
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use Call, Created do |env1, b2|
-          #   if !env1[:result]
-          #     b2.use MessageNotCreated
-          #     next
-          #   end
-
-          #   b2.use SetupPackageFiles
-          #   b2.use CheckAccessible
-          #   b2.use action_halt
-          #   b2.use ClearForwardedPorts
-          #   b2.use ClearSharedFolders
-          #   b2.use Export
-          #   b2.use PackageVagrantfile
-          #   b2.use Package
-          # end
         end
       end
 
       # This action just runs the provisioners on the machine.
       def self.action_provision
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use ConfigValidate
-          # b.use Call, Created do |env1, b2|
-          #   if !env1[:result]
-          #     b2.use MessageNotCreated
-          #     next
-          #   end
-
-          #   b2.use Call, IsRunning do |env2, b3|
-          #     if !env2[:result]
-          #       b3.use MessageNotRunning
-          #       next
-          #     end
-
-          #     b3.use CheckAccessible
-          #     b3.use Provision
-          #   end
-          # end
         end
       end
 
@@ -142,17 +45,6 @@ module VagrantPlugins
       # machine back up with the new configuration.
       def self.action_reload
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use Call, Created do |env1, b2|
-          #   if !env1[:result]
-          #     b2.use MessageNotCreated
-          #     next
-          #   end
-
-          #   b2.use ConfigValidate
-          #   b2.use action_halt
-          #   b2.use action_start
-          # end
         end
       end
 
@@ -160,40 +52,18 @@ module VagrantPlugins
       # suspended machines.
       def self.action_resume
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use Call, Created do |env, b2|
-          #   if env[:result]
-          #     b2.use CheckAccessible
-          #     b2.use EnvSet, :port_collision_repair => false
-          #     b2.use PrepareForwardedPortCollisionParams
-          #     b2.use HandleForwardedPortCollisions
-          #     b2.use Resume
-          #   else
-          #     b2.use MessageNotCreated
-          #   end
-          # end
         end
       end
 
       # This is the action that will exec into an SSH shell.
       def self.action_ssh
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use CheckCreated
-          # b.use CheckAccessible
-          # b.use CheckRunning
-          # b.use SSHExec
         end
       end
 
       # This is the action that will run a single SSH command.
       def self.action_ssh_run
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use CheckCreated
-          # b.use CheckAccessible
-          # b.use CheckRunning
-          # b.use SSHRun
         end
       end
 
@@ -201,34 +71,6 @@ module VagrantPlugins
       # A precondition of this action is that the VM exists.
       def self.action_start
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use ConfigValidate
-          # b.use Call, IsRunning do |env, b2|
-          #   # If the VM is running, then our work here is done, exit
-          #   if env[:result]
-          #     b2.use MessageAlreadyRunning
-          #     next
-          #   end
-
-          #   b2.use Call, IsSaved do |env2, b3|
-          #     if env2[:result]
-          #       # The VM is saved, so just resume it
-          #       b3.use action_resume
-          #       next
-          #     end
-
-          #     b3.use Call, IsPaused do |env3, b4|
-          #       if env3[:result]
-          #         b4.use Resume
-          #         next
-          #       end
-
-          #       # The VM is not saved, so we must have to boot it up
-          #       # like normal. Boot!
-          #       b4.use action_boot
-          #     end
-          #   end
-          # end
         end
       end
 
@@ -236,15 +78,6 @@ module VagrantPlugins
       # the virtual machine.
       def self.action_suspend
         Vagrant::Action::Builder.new.tap do |b|
-          # b.use CheckParallels
-          # b.use Call, Created do |env, b2|
-          #   if env[:result]
-          #     b2.use CheckAccessible
-          #     b2.use Suspend
-          #   else
-          #     b2.use MessageNotCreated
-          #   end
-          # end
         end
       end
 
@@ -257,55 +90,13 @@ module VagrantPlugins
           b.use Call, Created do |env, b2|
             # If the VM is NOT created yet, then do the setup steps
             if !env[:result]
-              b2.use CheckAccessible
-              b2.use HandleBoxUrl
-              b2.use Import
-              b2.use MatchMACAddress
             end
           end
           # b.use action_start
         end
       end
 
-      # autoload :Boot, File.expand_path("../action/boot", __FILE__)
-      autoload :CheckAccessible, File.expand_path("../action/check_accessible", __FILE__)
-      # autoload :CheckCreated, File.expand_path("../action/check_created", __FILE__)
-      # autoload :CheckGuestAdditions, File.expand_path("../action/check_guest_additions", __FILE__)
       autoload :CheckParallels, File.expand_path("../action/check_parallels", __FILE__)
-      # autoload :CheckRunning, File.expand_path("../action/check_running", __FILE__)
-      # autoload :CleanMachineFolder, File.expand_path("../action/clean_machine_folder", __FILE__)
-      # autoload :ClearForwardedPorts, File.expand_path("../action/clear_forwarded_ports", __FILE__)
-      # autoload :ClearNetworkInterfaces, File.expand_path("../action/clear_network_interfaces", __FILE__)
-      # autoload :ClearSharedFolders, File.expand_path("../action/clear_shared_folders", __FILE__)
-      autoload :Created, File.expand_path("../action/created", __FILE__)
-      # autoload :Customize, File.expand_path("../action/customize", __FILE__)
-      # autoload :Destroy, File.expand_path("../action/destroy", __FILE__)
-      # autoload :DestroyUnusedNetworkInterfaces, File.expand_path("../action/destroy_unused_network_interfaces", __FILE__)
-      # autoload :DiscardState, File.expand_path("../action/discard_state", __FILE__)
-      # autoload :Export, File.expand_path("../action/export", __FILE__)
-      # autoload :ForcedHalt, File.expand_path("../action/forced_halt", __FILE__)
-      # autoload :ForwardPorts, File.expand_path("../action/forward_ports", __FILE__)
-      autoload :Import, File.expand_path("../action/import", __FILE__)
-      # autoload :IsPaused, File.expand_path("../action/is_paused", __FILE__)
-      # autoload :IsRunning, File.expand_path("../action/is_running", __FILE__)
-      # autoload :IsSaved, File.expand_path("../action/is_saved", __FILE__)
-      autoload :MatchMACAddress, File.expand_path("../action/match_mac_address", __FILE__)
-      # autoload :MessageAlreadyRunning, File.expand_path("../action/message_already_running", __FILE__)
-      # autoload :MessageNotCreated, File.expand_path("../action/message_not_created", __FILE__)
-      # autoload :MessageNotRunning, File.expand_path("../action/message_not_running", __FILE__)
-      # autoload :MessageWillNotDestroy, File.expand_path("../action/message_will_not_destroy", __FILE__)
-      # autoload :Network, File.expand_path("../action/network", __FILE__)
-      # autoload :Package, File.expand_path("../action/package", __FILE__)
-      # autoload :PackageVagrantfile, File.expand_path("../action/package_vagrantfile", __FILE__)
-      # autoload :PrepareNFSSettings, File.expand_path("../action/prepare_nfs_settings", __FILE__)
-      # autoload :PrepareForwardedPortCollisionParams, File.expand_path("../action/prepare_forwarded_port_collision_params", __FILE__)
-      # autoload :PruneNFSExports, File.expand_path("../action/prune_nfs_exports", __FILE__)
-      # autoload :Resume, File.expand_path("../action/resume", __FILE__)
-      # autoload :SaneDefaults, File.expand_path("../action/sane_defaults", __FILE__)
-      # autoload :SetName, File.expand_path("../action/set_name", __FILE__)
-      # autoload :SetupPackageFiles, File.expand_path("../action/setup_package_files", __FILE__)
-      # autoload :ShareFolders, File.expand_path("../action/share_folders", __FILE__)
-      # autoload :Suspend, File.expand_path("../action/suspend", __FILE__)
     end
   end
 end
