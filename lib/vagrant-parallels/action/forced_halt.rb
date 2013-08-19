34ec2b086ce75626c137797df793577119e5ffd2
Youssef Shahin <yshahin@gmail.com>
2013-08-20 01:22:12 +0200

    force halt tweaking

--- lib/vagrant-parallels/action/forced_halt.rb
+++ lib/vagrant-parallels/action/forced_halt.rb
@@ -8,14 +8,10 @@ module VagrantPlugins
 
         def call(env)
           current_state = env[:machine].state.id
-          if current_state == :running || current_state == :gurumeditation
+          if current_state == :running
             env[:ui].info I18n.t("vagrant.actions.vm.halt.force")
             env[:machine].provider.driver.halt
           end
-
-          # Sleep for a second to verify that the VM properly
-          # cleans itself up. Silly VirtualBox.
-          sleep 1 if !env["vagrant.test"]
 
           @app.call(env)
         end
