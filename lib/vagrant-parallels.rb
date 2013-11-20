218e14800c6737622b54e3ff2980ec78a1b9b28e
Mikhail Zholobov <legal90@gmail.com>
2013-11-20 18:52:40 +0400

    'setup_i18n' and 'setup_logging' methods moved to the top level of module 'Parallels' and are called only one time there. Now they are available for any method of class 'Plugin' (such as 'provider', 'config', 'command', 'guest_capability' and other in future).

--- lib/vagrant-parallels.rb
+++ lib/vagrant-parallels.rb
@@ -14,5 +14,42 @@ module VagrantPlugins
     def self.source_root
       @source_root ||= Pathname.new(File.expand_path("../../", __FILE__))
     end
+
+    # This initializes the internationalization strings.
+    def self.setup_i18n
+      I18n.load_path << File.expand_path("locales/en.yml", Parallels.source_root)
+      I18n.reload!
+    end
+
+    # This sets up our log level to be whatever VAGRANT_LOG is.
+    def self.setup_logging
+      require "log4r"
+
+      level = nil
+      begin
+        level = Log4r.const_get(ENV["VAGRANT_LOG"].upcase)
+      rescue NameError
+        # This means that the logging constant wasn't found,
+        # which is fine. We just keep `level` as `nil`. But
+        # we tell the user.
+        level = nil
+      end
+
+      # Some constants, such as "true" resolve to booleans, so the
+      # above error checking doesn't catch it. This will check to make
+      # sure that the log level is an integer, as Log4r requires.
+      level = nil if !level.is_a?(Integer)
+
+      # Set the logging level on all "vagrant" namespaced
+      # logs as long as we have a valid level.
+      if level
+        logger = Log4r::Logger.new("vagrant_parallels")
+        logger.outputters = Log4r::Outputter.stderr
+        logger.level = level
+        logger = nil
+      end
+    end
+    Parallels.setup_logging
+    Parallels.setup_i18n
   end
 end
