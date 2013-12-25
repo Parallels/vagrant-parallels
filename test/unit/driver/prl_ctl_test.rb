074af76f7e0a20e988f360bcb4c514fdf668ebeb
Mikhail Zholobov <legal90@gmail.com>
2013-12-25 18:19:09 +0400

    Indentation fixed

--- test/unit/driver/prl_ctl_test.rb
+++ test/unit/driver/prl_ctl_test.rb
@@ -21,12 +21,12 @@ describe VagrantPlugins::Parallels::Driver::PrlCtl do
 
     it "exports VM to template" do
       subject.stub(:read_settings).with(tpl_name).
-          and_return({"ID" => tpl_uuid})
+        and_return({"ID" => tpl_uuid})
 
       subprocess.should_receive(:execute).
-          with("prlctl", "clone", uuid, "--name", an_instance_of(String), "--template", "--dst",
-               an_instance_of(String), an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "The VM has been successfully cloned"))
+        with("prlctl", "clone", uuid, "--name", an_instance_of(String), "--template", "--dst",
+             an_instance_of(String), an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "The VM has been successfully cloned"))
       subject.export("/path/to/template", tpl_name).should == tpl_uuid
     end
   end
@@ -37,8 +37,8 @@ describe VagrantPlugins::Parallels::Driver::PrlCtl do
       subject.stub(:read_settings).and_return({"Host Shared Folders" => shf_hash})
 
       subprocess.should_receive(:execute).exactly(2).times.
-          with("prlctl", "set", uuid, "--shf-host-del", an_instance_of(String), an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "Shared folder deleted"))
+        with("prlctl", "set", uuid, "--shf-host-del", an_instance_of(String), an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "Shared folder deleted"))
       subject.clear_shared_folders
     end
   end
@@ -46,15 +46,15 @@ describe VagrantPlugins::Parallels::Driver::PrlCtl do
   describe "halt" do
     it "stops the VM" do
       subprocess.should_receive(:execute).
-          with("prlctl", "stop", uuid, an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "VM has been halted gracefully"))
+        with("prlctl", "stop", uuid, an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "VM has been halted gracefully"))
       subject.halt
     end
 
     it "stops the VM force" do
       subprocess.should_receive(:execute).
-          with("prlctl", "stop", uuid, "--kill", an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "VM has been halted forcibly"))
+        with("prlctl", "stop", uuid, "--kill", an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "VM has been halted forcibly"))
       subject.halt(force=true)
     end
   end
@@ -62,8 +62,8 @@ describe VagrantPlugins::Parallels::Driver::PrlCtl do
   describe "start" do
     it "starts the VM" do
       subprocess.should_receive(:execute).
-          with("prlctl", "start", uuid, an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "VM started"))
+        with("prlctl", "start", uuid, an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "VM started"))
       subject.start
     end
   end
@@ -71,8 +71,8 @@ describe VagrantPlugins::Parallels::Driver::PrlCtl do
   describe "suspend" do
     it "suspends the VM" do
       subprocess.should_receive(:execute).
-          with("prlctl", "suspend", uuid, an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "VM suspended"))
+        with("prlctl", "suspend", uuid, an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "VM suspended"))
       subject.suspend
     end
   end
@@ -80,8 +80,8 @@ describe VagrantPlugins::Parallels::Driver::PrlCtl do
   describe "unregister" do
     it "suspends the VM" do
       subprocess.should_receive(:execute).
-          with("prlctl", "unregister", an_instance_of(String), an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "Specified VM unregistered"))
+        with("prlctl", "unregister", an_instance_of(String), an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "Specified VM unregistered"))
       subject.unregister("template_or_vm_uuid")
     end
   end
@@ -93,10 +93,10 @@ describe VagrantPlugins::Parallels::Driver::PrlCtl do
 
     it "rises ParallelsInstallIncomplete exception when output is invalid" do
       subprocess.should_receive(:execute).
-          with("prlctl", "--version", an_instance_of(Hash)).
-          and_return(subprocess_result(stdout: "Some incorrect value has been returned!"))
+        with("prlctl", "--version", an_instance_of(Hash)).
+        and_return(subprocess_result(stdout: "Some incorrect value has been returned!"))
       expect { subject.version }.
-          to raise_error(VagrantPlugins::Parallels::Errors::ParallelsInstallIncomplete)
+        to raise_error(VagrantPlugins::Parallels::Errors::ParallelsInstallIncomplete)
     end
   end
 end
