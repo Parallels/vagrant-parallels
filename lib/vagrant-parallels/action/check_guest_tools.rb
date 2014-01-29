969fd3bd2d4ae13aeb77cea84bd7e22413f2fe56
Mikhail Zholobov <legal90@gmail.com>
2014-01-29 23:59:47 +0400

    Action 'CheckGuestTools': text notice added

--- lib/vagrant-parallels/action/check_guest_tools.rb
+++ lib/vagrant-parallels/action/check_guest_tools.rb
@@ -7,6 +7,8 @@ module VagrantPlugins
         end
 
         def call(env)
+          env[:ui].output(I18n.t("vagrant_parallels.parallels.checking_guest_tools"))
+
           tools_version = env[:machine].provider.driver.read_guest_tools_version
           if !tools_version
             env[:ui].warn I18n.t("vagrant_parallels.actions.vm.check_guest_tools.not_detected")
@@ -14,8 +16,8 @@ module VagrantPlugins
             pd_version = env[:machine].provider.driver.version
             unless pd_version.start_with? tools_version
               env[:ui].warn(I18n.t("vagrant_parallels.actions.vm.check_guest_tools.version_mismatch",
-                                   tools_version: tools_version,
-                                   parallels_version: pd_version))
+                                   :tools_version => tools_version,
+                                   :parallels_version => pd_version))
             end
           end
 
