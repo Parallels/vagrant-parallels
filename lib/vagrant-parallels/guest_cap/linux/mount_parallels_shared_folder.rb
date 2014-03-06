b9a2399e148be7660c51501da2015e31b96914cb
racktear <mail@racktear.com>
2014-03-07 00:33:12 +0400

    Use 'shell_expand_guest_path' guest capability to expand "~/" in shared folder mounts
    This was copied from the official NFS mount capability

--- lib/vagrant-parallels/guest_cap/linux/mount_parallels_shared_folder.rb
+++ lib/vagrant-parallels/guest_cap/linux/mount_parallels_shared_folder.rb
@@ -4,25 +4,29 @@ module VagrantPlugins
       class MountParallelsSharedFolder
 
         def self.mount_parallels_shared_folder(machine, name, guestpath, options)
+          # Expand the guest path so we can handle things like "~/vagrant"
+          expanded_guest_path = machine.guest.capability(
+            :shell_expand_guest_path, guestpath)
+
           machine.communicate.tap do |comm|
             # clear prior symlink
-            if comm.test("test -L \"#{guestpath}\"", :sudo => true)
-              comm.sudo("rm \"#{guestpath}\"")
+            if comm.test("test -L \"#{expanded_guest_path}\"", :sudo => true)
+              comm.sudo("rm \"#{expanded_guest_path}\"")
             end
 
             # clear prior directory if exists
-            if comm.test("test -d \"#{guestpath}\"", :sudo => true)
-              comm.sudo("rm -Rf \"#{guestpath}\"")
+            if comm.test("test -d \"#{expanded_guest_path}\"", :sudo => true)
+              comm.sudo("rm -Rf \"#{expanded_guest_path}\"")
             end
 
             # create intermediate directories if needed
-            intermediate_dir = File.dirname(guestpath)
+            intermediate_dir = File.dirname(expanded_guest_path)
             if !comm.test("test -d \"#{intermediate_dir}\"", :sudo => true)
               comm.sudo("mkdir -p \"#{intermediate_dir}\"")
             end
 
             # finally make the symlink
-            comm.sudo("ln -s \"/media/psf/#{name}\" \"#{guestpath}\"")
+            comm.sudo("ln -s \"/media/psf/#{name}\" \"#{expanded_guest_path}\"")
           end
         end
       end
