735fbaa07412b573015637439d2f2b1c1f951104
Mikhail Zholobov <legal90@gmail.com>
2014-04-22 23:50:34 +0400

    tests/unit: added tests for 'set_power_consumption_mode' driver method

--- test/unit/support/shared/pd_driver_examples.rb
+++ test/unit/support/shared/pd_driver_examples.rb
@@ -217,6 +217,26 @@ shared_examples "parallels desktop driver" do |options|
     end
   end
 
+  describe "set_power_consumption_mode" do
+    it "turns 'longer-battery-life' on" do
+      subprocess.should_receive(:execute).
+        with("prlctl", "set", uuid, "--longer-battery-life", "on",
+             an_instance_of(Hash)).
+        and_return(subprocess_result(exit_code: 0))
+
+      subject.set_power_consumption_mode(true)
+    end
+
+    it "turns 'longer-battery-life' off" do
+      subprocess.should_receive(:execute).
+        with("prlctl", "set", uuid, "--longer-battery-life", "off",
+             an_instance_of(Hash)).
+        and_return(subprocess_result(exit_code: 0))
+
+      subject.set_power_consumption_mode(false)
+    end
+  end
+
   describe "start" do
     it "starts the VM" do
       subprocess.should_receive(:execute).
