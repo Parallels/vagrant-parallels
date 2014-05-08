a99639e311d33039243ae05978bdf478c9fe23d4
Mikhail Zholobov <legal90@gmail.com>
2014-05-08 14:51:07 +0400

    Unit tests modified

--- test/unit/synced_folder_test.rb
+++ test/unit/synced_folder_test.rb
@@ -1,15 +1,22 @@
 require "vagrant"
 require_relative "base"
 
+require VagrantPlugins::Parallels.source_root.join("lib/vagrant-parallels/config")
 require VagrantPlugins::Parallels.source_root.join('lib/vagrant-parallels/synced_folder')
 
 describe VagrantPlugins::Parallels::SyncedFolder do
   let(:machine) do
     double("machine").tap do |m|
+      m.stub(provider_config: VagrantPlugins::Parallels::Config.new)
+      m.stub(provider_name: :parallels)
     end
   end
 
   subject { described_class.new }
+
+  before do
+    machine.provider_config.finalize!
+  end
 
   describe "usable" do
     it "should be with parallels provider" do
@@ -20,6 +27,11 @@ describe VagrantPlugins::Parallels::SyncedFolder do
     it "should not be with another provider" do
       machine.stub(provider_name: :virtualbox)
       subject.should_not be_usable(machine)
+    end
+
+    it "should not be usable if not functional psf" do
+      machine.provider_config.functional_psf = false
+      expect(subject).to_not be_usable(machine)
     end
   end
 
