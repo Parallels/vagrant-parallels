4c29f80386024671a2c0b7c359cd265f81ed7ee2
Mikhail Zholobov <legal90@gmail.com>
2015-08-25 16:25:11 +0300

    Move #setup_i18n and #setup_logging to plugin class

--- lib/vagrant-parallels/plugin.rb
+++ lib/vagrant-parallels/plugin.rb
@@ -21,7 +21,11 @@ module VagrantPlugins
       EOF
 
       provider(:parallels, parallel: true, priority: 7) do
-        require File.expand_path("../provider", __FILE__)
+        # Setup logging and i18n
+        setup_logging
+        setup_i18n
+
+        require_relative 'provider'
         Provider
       end
 
@@ -80,9 +84,41 @@ module VagrantPlugins
         SyncedFolder
       end
 
-    end
+      # This initializes the internationalization strings.
+      def self.setup_i18n
+        I18n.load_path << File.expand_path('locales/en.yml', Parallels.source_root)
+        I18n.reload!
+      end
 
-    autoload :Action, File.expand_path("../action", __FILE__)
+      # This sets up our log level to be whatever VAGRANT_LOG is.
+      def self.setup_logging
+        require 'log4r'
+
+        level = nil
+        begin
+          level = Log4r.const_get(ENV['VAGRANT_LOG'].upcase)
+        rescue NameError
+          # This means that the logging constant wasn't found,
+          # which is fine. We just keep `level` as `nil`. But
+          # we tell the user.
+          level = nil
+        end
+
+        # Some constants, such as "true" resolve to booleans, so the
+        # above error checking doesn't catch it. This will check to make
+        # sure that the log level is an integer, as Log4r requires.
+        level = nil if !level.is_a?(Integer)
+
+        # Set the logging level on all "vagrant" namespaced
+        # logs as long as we have a valid level.
+        if level
+          logger = Log4r::Logger.new('vagrant_parallels')
+          logger.outputters = Log4r::Outputter.stderr
+          logger.level = level
+          logger = nil
+        end
+      end
+    end
 
     # Drop some autoloads in here to optimize the performance of loading
     # our drivers only when they are needed.
