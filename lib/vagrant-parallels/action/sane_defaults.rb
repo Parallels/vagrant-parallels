d270d18108be3ca074b24a56ca81de266bb704bc
Mikhail Zholobov <legal90@gmail.com>
2015-09-25 00:15:55 +0300

    action/sane_defaults: Remove set_power_consumption method
    It will be refactored further.

--- lib/vagrant-parallels/action/sane_defaults.rb
+++ lib/vagrant-parallels/action/sane_defaults.rb
@@ -14,31 +14,7 @@ module VagrantPlugins
           # helpers.
           @env = env
 
-          if env[:machine].provider.pd_version_satisfies?('>= 9')
-            @logger.info('Setting the power consumption mode...')
-            set_power_consumption
-          end
-
           @app.call(env)
-        end
-
-        private
-
-        def set_power_consumption
-          # Optimization of power consumption is defined by "Longer Battery Life" state.
-          vm_settings = @env[:machine].provider.driver.read_settings
-
-          old_val = vm_settings.fetch('Longer battery life') == 'on' ? true : false
-          new_val = @env[:machine].provider_config.optimize_power_consumption
-
-          if old_val == new_val
-            @logger.info('Skipping power consumption method because it is already set')
-          else
-            mode = new_val ? 'Longer battery life' : 'Better Performance'
-            @env[:ui].info I18n.t('vagrant_parallels.parallels.power_consumption.set_mode',
-                                  mode: mode)
-            @env[:machine].provider.driver.set_power_consumption_mode(new_val)
-          end
         end
       end
     end
