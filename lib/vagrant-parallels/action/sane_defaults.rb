ae2f7b993c2f454665562785e0c65cc0a3d95d9f
Mikhail Zholobov <legal90@gmail.com>
2015-09-25 00:17:01 +0300

    action/sane_defaults: Manage VM default settings

--- lib/vagrant-parallels/action/sane_defaults.rb
+++ lib/vagrant-parallels/action/sane_defaults.rb
@@ -14,7 +14,57 @@ module VagrantPlugins
           # helpers.
           @env = env
 
+          settings = default_settings
+
+          @app.call(env) if settings.empty?
+          @env[:ui].info I18n.t('vagrant_parallels.actions.vm.sane_defaults.setting')
+
+          default_settings.each do |setting, value|
+            @env[:machine].provider.driver.execute_prlctl(
+              'set', @env[:machine].id, "--#{setting.to_s.gsub('_','-')}", value)
+          end
+
           @app.call(env)
+        end
+
+        private
+
+        def default_settings
+          settings = {}
+
+          return settings if @env[:machine].provider.pd_version_satisfies?('< 9')
+          settings.merge!(
+            startup_view: 'same',
+            on_shutdown: 'close',
+            on_window_close: 'keep-running',
+            auto_share_camera: 'off',
+            smart_guard: 'off',
+            longer_battery_life: 'on'
+          )
+
+          # Check the legacy option
+          if !@env[:machine].provider_config.optimize_power_consumption
+            settings[:longer_battery_life] = 'off'
+          end
+
+          return settings  if @env[:machine].provider.pd_version_satisfies?('< 10.1.2')
+          settings.merge!(
+            shared_cloud: 'off',
+            shared_profile: 'off',
+            smart_mount: 'off',
+            sh_app_guest_to_host: 'off',
+            sh_app_host_to_guest: 'off',
+            time_sync: 'off'
+          )
+
+          return settings if @env[:machine].provider.pd_version_satisfies?('< 11')
+          settings.merge!(
+            startup_view: 'headless',
+            time_sync: 'on',
+            disable_timezone_sync: 'on'
+          )
+
+          settings
         end
       end
     end
