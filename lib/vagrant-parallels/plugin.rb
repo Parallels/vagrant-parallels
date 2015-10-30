1d987d5d58e35731230bd08d02734a60f8ed452f
Mikhail Zholobov <legal90@gmail.com>
2015-10-30 23:38:28 +0200

    Fix Vagrant version comparing
    We should compare version instances, but not strings.
    Otherwise there will be a messy: "1.10.0" < "1.8.0"

--- lib/vagrant-parallels/plugin.rb
+++ lib/vagrant-parallels/plugin.rb
@@ -6,8 +6,8 @@ end
 
 # This is a sanity check to make sure no one is attempting to install
 # this into an early Vagrant version.
-if Vagrant::VERSION < '1.5.0'
-  raise 'The Vagrant Parallels plugin is only compatible with Vagrant 1.5+'
+if Gem::Version.new(Vagrant::VERSION).release < Gem::Version.new('1.5.0')
+  raise 'The installed version of Vagrant Parallels plugin is only compatible with Vagrant 1.5+'
 end
 
 module VagrantPlugins
