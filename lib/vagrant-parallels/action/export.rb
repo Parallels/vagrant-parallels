baeceb6c38bb5ae3a7f785ff2ffd605fc00ae829
Mikhail Zholobov <legal90@gmail.com>
2015-11-04 18:58:09 +0200

    action/export: Get rid of instance variables
    They are too messy. It's much better to use explicit arguments here.

--- lib/vagrant-parallels/action/export.rb
+++ lib/vagrant-parallels/action/export.rb
@@ -8,30 +8,28 @@ module VagrantPlugins
         end
 
         def call(env)
-          @env = env
           if env[:machine].state.id != :stopped
             raise Vagrant::Errors::VMPowerOffToPackage
           end
 
-          export
-          compact_template
-          unregister_template
+          export(env)
+          compact_template(env)
+          unregister_template(env)
 
           @app.call(env)
         end
 
         def recover(env)
-          @env = env
-          unregister_template
+          unregister_template(env)
         end
 
         private
 
-        def box_template_name
+        def box_template_name(env)
           # Use configured name if it is specified, or generate the new one
-          name = @env[:machine].provider_config.name
+          name = env[:machine].provider_config.name
           if !name
-            name = "#{@env[:root_path].basename.to_s}_#{@env[:machine].name}"
+            name = "#{env[:root_path].basename.to_s}_#{env[:machine].name}"
             name.gsub!(/[^-a-z0-9_]/i, '')
           end
 
@@ -39,7 +37,7 @@ module VagrantPlugins
 
           # Ensure that the name is not in use
           ind = 0
-          while @env[:machine].provider.driver.read_vms.has_key?(tpl_name)
+          while env[:machine].provider.driver.read_vms.has_key?(tpl_name)
             ind += 1
             tpl_name = "#{name}_box_#{ind}"
           end
@@ -47,49 +45,49 @@ module VagrantPlugins
           tpl_name
         end
 
-        def export
-          @env[:ui].info I18n.t('vagrant.actions.vm.export.exporting')
+        def export(env)
+          env[:ui].info I18n.t('vagrant.actions.vm.export.exporting')
 
           options = {
             template: true,
-            dst: @env['export.temp_dir'].to_s
+            dst: env['export.temp_dir'].to_s
           }
 
-          @env[:package_box_id] = @env[:machine].provider.driver.clone_vm(
-            @env[:machine].id, options) do |progress|
-            @env[:ui].clear_line
-            @env[:ui].report_progress(progress, 100, false)
+          env[:package_box_id] = env[:machine].provider.driver.clone_vm(
+            env[:machine].id, options) do |progress|
+            env[:ui].clear_line
+            env[:ui].report_progress(progress, 100, false)
 
-            # # If we got interrupted, then the import could have been interrupted.
-            # Just rise an exception and then 'recover' will be called to cleanup.
-            raise Vagrant::Errors::VagrantInterrupt if @env[:interrupted]
+            # If we got interrupted, then rise an exception and 'recover'
+            # will be called to cleanup.
+            raise Vagrant::Errors::VagrantInterrupt if env[:interrupted]
           end
 
           # Set the template name
-          tpl_name = box_template_name
-          @env[:machine].provider.driver.set_name(@env[:package_box_id], tpl_name)
+          tpl_name = box_template_name(env)
+          env[:machine].provider.driver.set_name(env[:package_box_id], tpl_name)
 
           # Clear the line a final time so the next data can appear
           # alone on the line.
-          @env[:ui].clear_line
+          env[:ui].clear_line
         end
 
-        def compact_template
-          @env[:ui].info I18n.t('vagrant_parallels.actions.vm.export.compacting')
-          @env[:machine].provider.driver.compact(@env[:package_box_id]) do |progress|
-            @env[:ui].clear_line
-            @env[:ui].report_progress(progress, 100, false)
+        def compact_template(env)
+          env[:ui].info I18n.t('vagrant_parallels.actions.vm.export.compacting')
+          env[:machine].provider.driver.compact(env[:package_box_id]) do |progress|
+            env[:ui].clear_line
+            env[:ui].report_progress(progress, 100, false)
           end
 
           # Clear the line a final time so the next data can appear
           # alone on the line.
-          @env[:ui].clear_line
+          env[:ui].clear_line
         end
 
-        def unregister_template
-          return if !@env[:package_box_id]
-          @logger.info("Unregister the box template: '#{@env[:package_box_id]}'")
-          @env[:machine].provider.driver.unregister(@env[:package_box_id])
+        def unregister_template(env)
+          return if !env[:package_box_id]
+          @logger.info("Unregister the box template: '#{env[:package_box_id]}'")
+          env[:machine].provider.driver.unregister(env[:package_box_id])
         end
       end
     end
