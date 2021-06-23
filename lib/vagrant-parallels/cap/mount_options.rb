702b1200f630dcb4377a457052881f1c5c37ec50
Mikhail Zholobov <legal90@gmail.com>
2021-06-23 07:47:37 +0200

    [mount_options] Add "_netdev" mount option by default
    That will prevent VM from boot failure if the shared folder (filesystem) is not available.
    
    From the `man mount`:
        _netdev The filesystem resides on a device that requires network access (used to prevent the system from
        attempting to mount these filesystems until the network has been enabled on the system).

--- lib/vagrant-parallels/cap/mount_options.rb
+++ lib/vagrant-parallels/cap/mount_options.rb
@@ -22,6 +22,7 @@ module VagrantPlugins
 
           mount_options << "uid=#{mount_uid}"
           mount_options << "gid=#{mount_gid}"
+          mount_options << "_netdev"
           mount_options = mount_options.join(',')
           return mount_options, mount_uid, mount_gid
         end
