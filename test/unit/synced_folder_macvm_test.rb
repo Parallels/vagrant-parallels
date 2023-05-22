9e9f3d8f2cc9b9c0013997c0e86518a52a05f267
Mikhail Zholobov <legal90@gmail.com>
2023-05-22 11:06:13 +0200

    Update unit test for synced folders
    It's related to the new class for macvm guests: https://github.com/Parallels/vagrant-parallels/pull/448

--- test/unit/synced_folder_macvm_test.rb
+++ test/unit/synced_folder_macvm_test.rb
@@ -2,22 +2,17 @@ require 'vagrant'
 require_relative 'base'
 
 require VagrantPlugins::Parallels.source_root.join('lib/vagrant-parallels/config')
-require VagrantPlugins::Parallels.source_root.join('lib/vagrant-parallels/synced_folder')
+require VagrantPlugins::Parallels.source_root.join('lib/vagrant-parallels/synced_folder_macvm')
 
-describe VagrantPlugins::Parallels::SyncedFolder do
+describe VagrantPlugins::Parallels::SyncedFolderMacVM do
   let(:machine) do
     double('machine').tap do |m|
-      allow(m).to receive_messages(provider_config: VagrantPlugins::Parallels::Config.new)
       allow(m).to receive_messages(provider_name: :parallels)
-      allow(VagrantPlugins::Parallels::Util::Common).to receive(:is_macvm).with(m).and_return(false)
+      allow(VagrantPlugins::Parallels::Util::Common).to receive(:is_macvm).with(m).and_return(true)
     end
   end
 
   subject { described_class.new }
-
-  before do
-    machine.provider_config.finalize!
-  end
 
   describe 'usable' do
     it 'should be with parallels provider' do
@@ -30,14 +25,9 @@ describe VagrantPlugins::Parallels::SyncedFolder do
       expect(subject).not_to be_usable(machine)
     end
 
-    it 'should not be with macvm guest' do
+    it 'should be with macvm guest' do
       allow(VagrantPlugins::Parallels::Util::Common).to receive(:is_macvm).with(machine).and_return(true)
-      expect(subject).not_to be_usable(machine)
-    end
-
-    it 'should not be usable if not functional psf' do
-      machine.provider_config.functional_psf = false
-      expect(subject).to_not be_usable(machine)
+      expect(subject).to be_usable(machine)
     end
   end
 
